5c68fb6328f208a90f552104418909dd449d791a
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-02-12 15:40:46 +0200

    added roles to access token

--- src/middleware/protect.ts
+++ src/middleware/protect.ts
@@ -2,26 +2,33 @@ import { NextFunction, Request, Response } from "express";
 import { Database } from "../data-source";
 import { User } from "../entities";
 import { UserService } from "../services";
-import { decode } from "jsonwebtoken";
+import { decode, SigningKeyCallback, verify } from "jsonwebtoken";
+import { promisify } from "util";
+
 /**
  * Validate that the user is logged in, and has a valid token that is not expired.
  */
 export const protect = async (req: Request, res: Response, next: NextFunction) => {
   if (req.headers.authorization && req.headers.authorization.toLowerCase().startsWith("bearer")) {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = decode(token) as { id: number, roles: string[], iat: number, exp: number };
+    const token: string = req.headers.authorization.split(" ")[1];
+    const asyncVerifyToken = promisify(verify) as (token: string, secret: string) => Promise<SigningKeyCallback>;
 
-    const service = new UserService(Database.getRepository(User));
-    const user = await service.findUser(decodedToken?.id);
+    try {
+      const decoded = await asyncVerifyToken(token, process.env.ACCESS_TOKEN_SECRET_KEY!) as any as { id: number, roles: string[], iat: number, exp: number };;
+      const service = new UserService(Database.getRepository(User));
+      const user = await service.findUser(decoded.id);
+      if (user) {
+        delete user.password;
+        req.user = user;
+        next();
+      } else {
+        return next(new Error('Invalid token! Please log in again.'));
+      }
+    } catch (error) {
+      console.log(error);
+      return next(new Error('Unauthenticated! Something is wrong with your token!'));
 
-    if (user) {
-      delete user.password;
-      req.user = user;
-      next();
-    } else {
-      return next(new Error('Invalid token! Please log in again.'));
     }
-
   } else {
     const error = new Error('Unauthenticated! Please log in and try again.');
     return next(error);
